a068482fe973f36b3fb0adb0a92a2eba2e4a04bc
Marco <arboleda.marco@gmail.com>
2020-04-03 20:49:23 -0400

    Make game work without prompt, via DOM

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,20 @@
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    let computerSelection;
+
     playerSelection = playerSelection.toLowerCase();
+    computerSelectionID = Math.floor(Math.random() * 3);
+
+    switch(computerSelectionID) {
+        case 0:
+            computerSelection = "rock";
+            break;
+        case 1:
+            computerSelection = "scissors";
+            break;
+        case 2:
+            computerSelection = "paper";
+            break;
+    }
     
     if (playerSelection == computerSelection) {
         return "It's a tie!";
@@ -21,41 +36,54 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+function addScore(resultsString) {
+    if (resultsString.substr(0,8) == "You Win!") {
+        playerPoints++;
+    } else if (resultsString.substr(0,9) == "You Lose!") {
+        computerPoints++;
+    }
+}
+
+function displayWinningMessage(playerPoints, computerPoints) {
+    if (playerPoints > computerPoints) {
+        results.textContent = "Congratulations, you won the game!";
+    } else if (playerPoints < computerPoints) {
+        results.textContent = "YOU LOSE.";
+    } else {
+        results.textContent = "It's a tie!";
+    }
+}
+
 let playerPoints = 0;
 let computerPoints = 0;
-let message;
 let computerSelectionID;
 let computerSelection;
+let playerSelection;
 const results = document.querySelector("#results");
 const rockButton = document.querySelector("#rock");
 const scissorsButton = document.querySelector("#scissors");
 const paperButton = document.querySelector("#paper");
 
-for (i = 0; i < 5; i++) {
-    computerSelectionID = Math.floor(Math.random() * 3);
-    switch(computerSelectionID) {
-        case 0:
-            computerSelection = "rock";
-            break;
-        case 1:
-            computerSelection = "scissors";
-            break;
-        case 2:
-            computerSelection = "paper";
-            break;
+rockButton.addEventListener("click", () => {
+    results.textContent = playRound("rock");
+    addScore(results.textContent);
+    if (playerPoints == 5 || computerPoint == 5) {
+        displayWinningMessage(playerPoints, computerPoints);
     }
-    results.textContent = message;
-    if (message.substr(0,8) == "You Win!") {
-        playerPoints++;
-    } else if (message.substr(0,9) == "You Lose!") {
-        computerPoints++;
-    }
-}
+});
 
-if (playerPoints > computerPoints) {
-    results.textContent = "Congratulations, you won the game!";
-} else if (playerPoints < computerPoints) {
-    results.textContent = "YOU LOSE.";
-} else {
-    results.textContent = "It's a tie!";
-}
+scissorsButton.addEventListener("click", () => {
+    results.textContent = playRound("scissors");
+    addScore(results.textContent);
+    if (playerPoints == 5 || computerPoint == 5) {
+        displayWinningMessage(playerPoints, computerPoints);
+    }
+});
+
+paperButton.addEventListener("click", () => {
+    results.textContent = playRound("paper");
+    addScore(results.textContent);
+    if (playerPoints == 5 || computerPoint == 5) {
+        displayWinningMessage(playerPoints, computerPoints);
+    }
+});
